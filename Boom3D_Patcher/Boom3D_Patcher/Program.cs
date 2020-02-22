ce322c75e24a7d8d52d6b92b3f774f0178b54b1c
DecodeMehh <43712252+DecodeMehh@users.noreply.github.com>
2020-02-22 13:29:33 +0100

    Initial Release

---
+++ Boom3D_Patcher/Boom3D_Patcher/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+
+namespace Boom3D_Patcher
+{
+    internal static class Program
+    {
+        private static bool _isPatched = false;
+
+        public static void Main()
+        {
+            Console.Title = "Boom3D Patcher";
+
+            Console.WriteLine("Enter Boom3D.exe path: ");
+            var path = Console.ReadLine()?.Replace("\"", "");
+            var module = ModuleDefMD.Load(path);
+
+            if (IsPacked(module))
+            {
+                Console.WriteLine("Compressor detected, please remove it before patching the file.");
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            foreach (var type in module.GetTypes())
+            {
+                // Find Class PersistentSettings
+                if (!type.FullName.Contains("PersistentSettings")) continue;
+                Console.WriteLine($"Successfully got {type.FullName}");
+
+                foreach (var method in type.Methods)
+                {
+                    // Find Method get_IsPurchased
+                    if (!method.FullName.Contains("get_IsPurchased")) continue;
+                    Console.WriteLine($"Successfully got {method.FullName}\nRewriting existing OpCodes...");
+
+                    /*
+                     * Clear actual OpCodes and Add return true
+                     * ldc.i4.1
+                     * ret
+                     */
+                    method.Body.Instructions.Clear();
+                    method.Body.Instructions.Add(OpCodes.Ldc_I4_1.ToInstruction());
+                    method.Body.Instructions.Add(OpCodes.Ret.ToInstruction());
+                    _isPatched = true;
+                }
+            }
+
+            if (_isPatched)
+            {
+                Console.WriteLine("Rewriting done, saving file...");
+                var fileName = Path.GetFileName(path);
+
+                try
+                {
+                    module.Write(fileName);
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine($"Failed to save file. ({err.Message})");
+                }
+
+                Console.WriteLine($"File saved ({fileName}).");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Method not found, failed to patch.");
+                Console.ReadKey();
+            }
+        }
+
+        /// <summary>
+        /// Detect if file is using Compressor from ConfuserEx
+        /// </summary>
+        /// <param name="module"></param>
+        /// <returns></returns>
+        private static bool IsPacked(ModuleDefMD module)
+        {
+            for (uint rid = 1; rid <= module.Metadata.TablesStream.FileTable.Rows; ++rid)
+            {
+                module.TablesStream.TryReadFileRow(rid, out var row);
+                string name = module.StringsStream.ReadNoNull(row.Name);
+                if (name != "koi") continue;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Boom3D_Patcher
-A simple patcher for Boom3D
+[![Build Status](https://img.shields.io/travis/com/Mrakovic-ORG/Boom3D_Patcher?style=for-the-badge)](https://travis-ci.com/Mrakovic-ORG/Boom3D_Patcher)
+
+An simple patcher for [Boom3D](https://store.steampowered.com/app/1071100/Boom_3D/)
+
+## Features
+- Detects if file has Compressor from ConfuserEx
+- Automatically patch file
+
+## Build
+```SH
+git clone https://github.com/Mrakovic-ORG/Boom3D_Patcher --recurse
+cd Boom3D_Patcher\Boom3D_Patcher
+dotnet build
+```
+
+## In action
+![in-action](images/in-action.gif)
+
+## Disclaimer
+This project has been made for educational purpose, Cracking/Deobfuscating/Un-protecting a licensed software is illegal, This project does not deobfuscate/un-protect the software.
+
+## Credits
+[0xd4d](https://github.com/0xd4d) for the awesome work he brings to .NET developers.
+
+License
+----
+
+MIT
